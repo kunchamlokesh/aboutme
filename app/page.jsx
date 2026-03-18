1b87f29bed809fb9d1362bbd3e4d853f56ee8a0b
Kuncham Lokesh <lokeshtv17@gmail.com>
2026-03-18 16:16:59 +0530

    Create basic Next.js homepage content

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js Page</h1>
+      <p>This is a basic home page created using Next.js framework.</p>
+    </div>
+  );
+}
